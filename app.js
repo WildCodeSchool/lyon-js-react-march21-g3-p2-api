f09804f13aa2396a3e95d187a0bf4c8aad5b7691
Brandon Monge <oorekan@users.noreply.github.com>
2021-04-27 08:56:16 +0200

    back initialization

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { PORT, CORS_ALLOWED_ORIGINS, inTestEnv } = require('./env');
+
+const app = express();
+
+// app settings
+app.set('x-powered-by', false); // for security
+
+const allowedOrigins = CORS_ALLOWED_ORIGINS.split(',');
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (origin === undefined || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// server setup
+app.listen(PORT, () => {
+  if (!inTestEnv) {
+    console.log(`Server running on port ${PORT}`);
+  }
+});
+
+// process setup : improves error reporting
+process.on('unhandledRejection', (error) => {
+  console.error('unhandledRejection', JSON.stringify(error), error.stack);
+  process.exit(1);
+});
+process.on('uncaughtException', (error) => {
+  console.error('uncaughtException', JSON.stringify(error), error.stack);
+  process.exit(1);
+});
+process.on('beforeExit', () => {
+  app.close((error) => {
+    if (error) console.error(JSON.stringify(error), error.stack);
+  });
+});
