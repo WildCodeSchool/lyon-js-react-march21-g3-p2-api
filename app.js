66764e5365e40d267c1b7d37af5b66e7ab8b123b
Brandon Monge <oorekan@users.noreply.github.com>
2021-05-05 14:24:34 +0200

    Updated back-API & add a get/post method

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const cors = require('cors');
 const { PORT, CORS_ALLOWED_ORIGINS, inTestEnv } = require('./env');
 
 const app = express();
+const connection = require('./db-config');
 
-// app settings
-app.set('x-powered-by', false); // for security
+app.use(express.json());
+
+connection.connect((err) =>
+  err
+    ? console.error(`error connecting: ${err.stack}`)
+    : console.log(`connected as id ${connection.threadId}`)
+);
+
+app.set('x-powered-by', false);
 
 const allowedOrigins = CORS_ALLOWED_ORIGINS.split(',');
 const corsOptions = {
@@ -21,14 +29,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// server setup
 app.listen(PORT, () => {
   if (!inTestEnv) {
     console.log(`Server running on port ${PORT}`);
   }
 });
 
-// process setup : improves error reporting
 process.on('unhandledRejection', (error) => {
   console.error('unhandledRejection', JSON.stringify(error), error.stack);
   process.exit(1);
@@ -41,4 +47,43 @@ process.on('beforeExit', () => {
   app.close((error) => {
     if (error) console.error(JSON.stringify(error), error.stack);
   });
+});
+
+app.get('/books/:book_id/reviews', (req, res) => {
+  const { book_id } = req.params;
+  connection
+    .promise()
+    .query('SELECT * FROM Reviews WHERE book_id= ?', [book_id])
+    .then(([results]) => {
+      if (results.length) res.send(results);
+      else res.sendStatus(404);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+app.post('/books/:book_id/reviews', (req, res) => {
+  const { user_name, message } = req.body;
+  const { book_id } = req.params;
+  connection
+    .promise()
+    .query(
+      'INSERT INTO Reviews (book_id, user_name, message) VALUES (?, ?, ?)',
+      [book_id, user_name, message]
+    )
+    .then(([results]) => {
+      const newMessage = {
+        id: results.insertId,
+        book_id,
+        user_name,
+        message,
+      };
+      res.send(newMessage);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
